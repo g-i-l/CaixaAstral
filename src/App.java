89c9286dde35ce2b8f49db5184b2a4b3a0f4f60f
g-i-l <gilaraujo417@gmail.com>
2023-08-26 12:46:03 -0300

    base main class

--- src/App.java
+++ src/App.java
@@ -5,12 +5,50 @@ import venda.pagamento.DebitSale;
 
 public class App {
     public static void main(String[] args) throws Exception {
-    AstralCashier astral = new AstralCashier(111.0);
+        AstralCashier astral = new AstralCashier(111.0);
+        Scanner s = new Scanner(System.in);
+        int option;
+        do {
+            System.out.println("Informe a opção:");
+            menu();
+            option = Integer.parseInt(s.nextLine());
+            switch (option) {
+                default:
+                    System.out.println("Não é uma opção");
+                    break;
+                case 1:
+                    System.out.println("Informe a Quantia:");
+                    astral.getDebito().incomeValue(Double.parseDouble(s.nextLine()));
+                    break;
+                case 2:
+                    System.out.println("Informe a Quantia:");
+                    astral.getDebito().withdrawValue(Double.parseDouble(s.nextLine()));
+                    break;
+                case 3:
+                    System.out.println("Informe a Quantia:");
+                    astral.getCredito().incomeValue(Double.parseDouble(s.nextLine()));
+                    break;
+                case 4:
+                    System.out.println("Informe a Quantia:");
+                    astral.getCredito().withdrawValue(Double.parseDouble(s.nextLine()));
+                    break;
+                case 5:
+                    System.out.println("Informe a Quantia:");
+                    astral.getDinheiro().incomeValue(Double.parseDouble(s.nextLine()));
+                    break;
+                case 6:
+                    System.out.println("Informe a Quantia:");
+                    astral.getDinheiro().withdrawValue(Double.parseDouble(s.nextLine()));
+                    break;
+                case 7:
+                    System.out.println(astral);
+                    break;
+            }
+        } while (option != 0);
+    };
 
-
-    }
-
-    public static void menu(){
-        System.out.println("1- Entrada Débito%n2-Retirada Débito%n3- Entrada Crédito%%n4- Retirada Crédito");
+    public static void menu() {
+        System.out.println("1- Entrada Débito\n2-Retirada Débito\n3- Entrada Crédito\n4- Retirada Crédito");
+        System.out.println("5- Entrada Dinheiro\n6- Retirada Dinheiro\n7- Resumo");
     }
 }
